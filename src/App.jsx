2f3e84a1ee77e8267778ee74af53b6210c75498c
YoYo <raccoon.sleeplittle@gmail.com>
2023-09-01 22:45:43 +0700

    prepare project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import TodoList from './components/TodoList'
+
+
+const App = () => {
+    return (
+        <>
+            <TodoList />
+        </>
+    )
+}
+
+export default App
